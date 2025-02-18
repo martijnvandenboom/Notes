3755b13e971a9cc08ae69186adf1d48aca1d0242
ing. M.A.C.M. (Martijn) van den Boom <martijn.vandenboom@icloud.com>
2025-02-18 18:52:19 +0100

    change to config.toml to remove the baseurl
    change to config.toml to remove the baseurl

--- public/js/preferred-lang.js
+++ public/js/preferred-lang.js
@@ -1,1 +1,21 @@
-(()=>{var n={fallback:"https://ingvdboom-notes.netlify.app/v1/en/",homes:{ar:"https://ingvdboom-notes.netlify.app/v1/ar/",en:"https://ingvdboom-notes.netlify.app/v1/en/",fr:"https://ingvdboom-notes.netlify.app/v1/fr/",ja:"https://ingvdboom-notes.netlify.app/v1/ja/",nl:"https://ingvdboom-notes.netlify.app/v1/nl/","zh-hans":"https://ingvdboom-notes.netlify.app/v1/zh-hans/","zh-hant":"https://ingvdboom-notes.netlify.app/v1/zh-hant/"}};(()=>{let t=navigator.language||navigator.userLanguage;if(t in n.homes){window.location.href=n.homes[t];return}let a=t.split("-");for(let o in n.homes)if(o.indexOf(a[0])===0){window.location.href=n.homes[o];return}window.location.href=n.fallback})();})();
+(() => {
+  // ns-hugo-params:<stdin>
+  var stdin_default = { fallback: "http://localhost:1313/en/", homes: { ar: "http://localhost:1313/ar/", en: "http://localhost:1313/en/", fr: "http://localhost:1313/fr/", ja: "http://localhost:1313/ja/", nl: "http://localhost:1313/nl/", "zh-hans": "http://localhost:1313/zh-hans/", "zh-hant": "http://localhost:1313/zh-hant/" } };
+
+  // <stdin>
+  (() => {
+    const lang = navigator.language || navigator.userLanguage;
+    if (lang in stdin_default.homes) {
+      window.location.href = stdin_default.homes[lang];
+      return;
+    }
+    const codes = lang.split("-");
+    for (let lang2 in stdin_default.homes) {
+      if (lang2.indexOf(codes[0]) === 0) {
+        window.location.href = stdin_default.homes[lang2];
+        return;
+      }
+    }
+    window.location.href = stdin_default.fallback;
+  })();
+})();
