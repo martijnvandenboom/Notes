bd6af77433d12baee2475c7b20f5ab44ea0d0af5
ing. M.A.C.M. (Martijn) van den Boom <martijn.vandenboom@icloud.com>
2025-02-21 11:35:43 +0100

    recaptcha alert message removal
    recaptcha alert message removal

---
+++ public/assets/init/bundle.min.b3f3baa188d2887a8160e1be5b6c6a5f467b69dff6e98c772b9991e0410ddc6b.js
@@ -0,0 +1,174 @@
+(() => {
+  var __defProp = Object.defineProperty;
+  var __defNormalProp = (obj, key, value) => key in obj ? __defProp(obj, key, { enumerable: true, configurable: true, writable: true, value }) : obj[key] = value;
+  var __publicField = (obj, key, value) => __defNormalProp(obj, typeof key !== "symbol" ? key + "" : key, value);
+
+  // ns-hugo-params:/Users/martijnvandenboom/Library/Caches/hugo_cache/modules/filecache/modules/pkg/mod/github.com/razonyang/hugo-theme-bootstrap@v1.12.0/assets/js/mode/index.ts
+  var mode_default = { analytics: {}, archive: {}, author: { avatar: "/images/Martijn_001.jpeg", bio: "</br>Computer Infrastructure Architect</br>ingenieur, docent en mentor</br></br>", company: "Consultancy", location: "<a href='https://www.google.com/maps/search/Netherlands'>Netherlands</a>", name: "ing. M.A.C.M. (Martijn) van den Boom", params: {}, social: { bitbucket: "martijnvandenboom", email: "ingmacmvandenboom@gmail.com", facebook: "macmvandenboom", github: "martijnvandenboom", gitlab: "ingvdboom", instagram: "ingmacmvandenboom", lastfm: "ingvdboom", linkedin: "macmvandenboom", patreon: "martijnvandenboom", paypal: "ingmacmvandenboom", pinterest: "macmvandenboom", stackoverflow: "13417146", telegram: "ingvdboom", website: "https://vandenboom.icu", x: "ingvdboom", xing: "MACMMartijn_vandenBoom", youtube: "channel/UCslXipyVBwsTe6UjhVNOz7A" } }, baseURL: "http://localhost:1313/", brand: "Notes", breadcrumb: true, carouselpostcount: 5, codeblock: { linenos: true }, color: "auto", contact: { recaptcha: { sitekey: "6Lc_qN0qAAAAAIbpHWZBW-5LQMIzZJ6_USShZ4Ax" } }, counttaxonomyposts: true, creativecommons: { by: true, nc: true }, customcss: [], customjs: [], dateformat: ":date_long", description: "Un th\xE8me de blog extr\xEAmement rapide, r\xE9actif et riche en fonctionnalit\xE9s pour Hugo", diagram: false, docs: { nav: { expand: false, reducefontsize: true } }, docsnav: { reducefontsize: true }, featuredpostcount: 5, feeds: {}, fixedheader: true, fontsize: {}, fullwidth: false, funding: { alipay: "https://razonyang.com/images/reward/alipay.webp", buymeacoffee: "ingvdboom", ko_fi: "martijnvandenboom", open_collective: "ing-m-a-c-m-martijn-van-den-boom", patreon: "martijnvandenboom", paypal: "ingmacmvandenboom", wechat: "https://razonyang.com/images/reward/wechat.webp" }, giscus: { category: "General", categoryid: "MDE4OkRpc2N1c3Npb25DYXRlZ29yeTMyODkzODg2", repo: "razonyang/hugo-theme-bootstrap", repoid: "MDEwOlJlcG9zaXRvcnkzMDQzMzI4NTM=" }, googleadsense: "", keywords: "Hugo, Bootstrap, Th\xE8me Blog", mainsections: ["posts", "docs", "animations"], palette: "purple", palettes: ["blue", "blue gray", "brown", "cyan", "green", "indigo", "orange", "pink", "purple", "red", "teal", "yellow"], pinnedpost: true, pinnedpostcount: 3, post: { featuredimage: true, imagetitleascaption: true, numberifyheadings: true, numberifyheadingsseparator: "." }, postdate: true, poweredby: true, pwa: { manifest: { short_name: "MvdB" }, precache: {} }, readingtime: true, recentpostcount: 5, relatedpostcount: 5, repo: { subpath: "exampleSite", url: "https://github.com/razonyang/hugo-theme-bootstrap" }, sass_transpiler: "libsass", search: { fuse: { minmatchcharlength: 3 }, indexpaginate: 100 }, searchbar: true, sidebar: {}, sidebartaxonomies: ["series", "categories", "tags", "authors"], siteverification: { baidu: "", baiduunion: "", bing: "", google: "", shenma: "", so: "", sogou: "" }, social: { bitbucket: "martijnvandenboom", email: "ingmacmvandenboom@gmail.com", facebook: "macmvandenboom", github: "martijnvandenboom", gitlab: "ingvdboom", instagram: "ingmacmvandenboom", lastfm: "ingvdboom", linkedin: "macmvandenboom", patreon: "martijnvandenboom", paypal: "ingmacmvandenboom", pinterest: "macmvandenboom", stackoverflow: "13417146", telegram: "ingvdboom", website: "https://ingvdboom.netlify.app", x: "ingvdboom", xing: "MACMMartijn_vandenBoom", youtube: "channel/UCslXipyVBwsTe6UjhVNOz7A" }, socialanimation: true, socialcolor: false, taxonomypaginate: 10, taxonomypostcount: 20, titlecase: true, titleseparator: "-", tocwordcount: 1, topappbar: { social: { github: "razonyang/hugo-theme-bootstrap", patreon: "martijnvandenboom", paypal: "ingmacmvandenboom", website: "https://www.vandenboom.icu", x: "ingvdboom" } }, viewer: true };
+
+  // ns-hugo-params:/Users/martijnvandenboom/Library/Caches/hugo_cache/modules/filecache/modules/pkg/mod/github.com/razonyang/hugo-theme-bootstrap@v1.12.0/assets/js/local-storage/index.ts
+  var local_storage_default = { analytics: {}, archive: {}, author: { avatar: "/images/Martijn_001.jpeg", bio: "</br>Computer Infrastructure Architect</br>ingenieur, docent en mentor</br></br>", company: "Consultancy", location: "<a href='https://www.google.com/maps/search/Netherlands'>Netherlands</a>", name: "ing. M.A.C.M. (Martijn) van den Boom", params: {}, social: { bitbucket: "martijnvandenboom", email: "ingmacmvandenboom@gmail.com", facebook: "macmvandenboom", github: "martijnvandenboom", gitlab: "ingvdboom", instagram: "ingmacmvandenboom", lastfm: "ingvdboom", linkedin: "macmvandenboom", patreon: "martijnvandenboom", paypal: "ingmacmvandenboom", pinterest: "macmvandenboom", stackoverflow: "13417146", telegram: "ingvdboom", website: "https://vandenboom.icu", x: "ingvdboom", xing: "MACMMartijn_vandenBoom", youtube: "channel/UCslXipyVBwsTe6UjhVNOz7A" } }, baseURL: "http://localhost:1313/", brand: "Notes", breadcrumb: true, carouselpostcount: 5, codeblock: { linenos: true }, color: "auto", contact: { recaptcha: { sitekey: "6Lc_qN0qAAAAAIbpHWZBW-5LQMIzZJ6_USShZ4Ax" } }, counttaxonomyposts: true, creativecommons: { by: true, nc: true }, customcss: [], customjs: [], dateformat: ":date_long", description: "Un th\xE8me de blog extr\xEAmement rapide, r\xE9actif et riche en fonctionnalit\xE9s pour Hugo", diagram: false, docs: { nav: { expand: false, reducefontsize: true } }, docsnav: { reducefontsize: true }, featuredpostcount: 5, feeds: {}, fixedheader: true, fontsize: {}, fullwidth: false, funding: { alipay: "https://razonyang.com/images/reward/alipay.webp", buymeacoffee: "ingvdboom", ko_fi: "martijnvandenboom", open_collective: "ing-m-a-c-m-martijn-van-den-boom", patreon: "martijnvandenboom", paypal: "ingmacmvandenboom", wechat: "https://razonyang.com/images/reward/wechat.webp" }, giscus: { category: "General", categoryid: "MDE4OkRpc2N1c3Npb25DYXRlZ29yeTMyODkzODg2", repo: "razonyang/hugo-theme-bootstrap", repoid: "MDEwOlJlcG9zaXRvcnkzMDQzMzI4NTM=" }, googleadsense: "", keywords: "Hugo, Bootstrap, Th\xE8me Blog", mainsections: ["posts", "docs", "animations"], palette: "purple", palettes: ["blue", "blue gray", "brown", "cyan", "green", "indigo", "orange", "pink", "purple", "red", "teal", "yellow"], pinnedpost: true, pinnedpostcount: 3, post: { featuredimage: true, imagetitleascaption: true, numberifyheadings: true, numberifyheadingsseparator: "." }, postdate: true, poweredby: true, pwa: { manifest: { short_name: "MvdB" }, precache: {} }, readingtime: true, recentpostcount: 5, relatedpostcount: 5, repo: { subpath: "exampleSite", url: "https://github.com/razonyang/hugo-theme-bootstrap" }, sass_transpiler: "libsass", search: { fuse: { minmatchcharlength: 3 }, indexpaginate: 100 }, searchbar: true, sidebar: {}, sidebartaxonomies: ["series", "categories", "tags", "authors"], siteverification: { baidu: "", baiduunion: "", bing: "", google: "", shenma: "", so: "", sogou: "" }, social: { bitbucket: "martijnvandenboom", email: "ingmacmvandenboom@gmail.com", facebook: "macmvandenboom", github: "martijnvandenboom", gitlab: "ingvdboom", instagram: "ingmacmvandenboom", lastfm: "ingvdboom", linkedin: "macmvandenboom", patreon: "martijnvandenboom", paypal: "ingmacmvandenboom", pinterest: "macmvandenboom", stackoverflow: "13417146", telegram: "ingvdboom", website: "https://ingvdboom.netlify.app", x: "ingvdboom", xing: "MACMMartijn_vandenBoom", youtube: "channel/UCslXipyVBwsTe6UjhVNOz7A" }, socialanimation: true, socialcolor: false, taxonomypaginate: 10, taxonomypostcount: 20, titlecase: true, titleseparator: "-", tocwordcount: 1, topappbar: { social: { github: "razonyang/hugo-theme-bootstrap", patreon: "martijnvandenboom", paypal: "ingmacmvandenboom", website: "https://www.vandenboom.icu", x: "ingvdboom" } }, viewer: true };
+
+  // ns-hugo-imp:/Users/martijnvandenboom/Library/Caches/hugo_cache/modules/filecache/modules/pkg/mod/github.com/razonyang/hugo-theme-bootstrap@v1.12.0/assets/js/local-storage/index.ts
+  var PathLocalStorage = class {
+    constructor(baseURL) {
+      this.baseURL = baseURL;
+      __publicField(this, "prefix", "hbs:");
+      if (baseURL.substring(0, 2) === "//") {
+        baseURL = "http:" + baseURL;
+      }
+      let url;
+      try {
+        url = new URL(baseURL);
+      } catch (e) {
+        url = new URL(baseURL, location.protocol + "//" + location.host);
+      }
+      const pathname = url.pathname.replace(/^(\/+)/, "").replace(/(\/+)$/, "");
+      if (pathname !== "") {
+        this.prefix += pathname.replace("/", "-") + ":";
+      }
+    }
+    getItem(key) {
+      return localStorage.getItem(this.prefix + key);
+    }
+    setItem(key, value) {
+      localStorage.setItem(this.prefix + key, value);
+    }
+    removeItem(key) {
+      localStorage.removeItem(this.prefix + key);
+    }
+  };
+  var local_storage_default2 = new PathLocalStorage(local_storage_default.baseURL);
+
+  // ns-hugo-imp:/Users/martijnvandenboom/Library/Caches/hugo_cache/modules/filecache/modules/pkg/mod/github.com/razonyang/hugo-theme-bootstrap@v1.12.0/assets/js/mode/index.ts
+  var MODE_AUTO = "auto";
+  var MODE_DARK = "dark";
+  var MODE_LIGHT = "light";
+  var modes = [MODE_AUTO, MODE_DARK, MODE_LIGHT];
+  var ModeToggle = class _ModeToggle {
+    constructor() {
+      // Cache key.
+      __publicField(this, "key", "mode");
+      // Current color mode.
+      __publicField(this, "mode");
+      __publicField(this, "items");
+      let mode = local_storage_default2.getItem(this.key);
+      if (!mode) {
+        mode = mode_default.color;
+      }
+      this.mode = modes.includes(mode) ? mode : MODE_AUTO;
+    }
+    run() {
+      this.setMode(this.mode);
+      window.addEventListener("load", () => {
+        this.initListeners();
+        this.active(this.mode);
+      });
+    }
+    initListeners() {
+      this.items = document.querySelectorAll(".mode-item");
+      this.items.forEach((ele) => {
+        ele.addEventListener("click", () => {
+          const mode = ele.getAttribute("data-color-mode");
+          this.setMode(mode);
+          this.active(mode);
+        });
+      });
+      window.matchMedia("(prefers-color-scheme: dark)").addListener((e) => {
+        if (this.isAuto()) {
+          this.setMode(e.matches ? "dark" : "light");
+        }
+      });
+    }
+    isAuto() {
+      return this.mode === MODE_AUTO;
+    }
+    // Active the relative HTML elements.
+    active(mode) {
+      var _a, _b;
+      this.mode = mode;
+      local_storage_default2.setItem(this.key, mode);
+      this.items.forEach((ele) => {
+        const classList = ele.querySelector(".dropdown-item").classList;
+        if (ele.getAttribute("data-color-mode") === mode) {
+          classList.add("active");
+        } else {
+          classList.remove("active");
+        }
+      });
+      const icon = (_a = document.querySelector('.mode-item[data-color-mode="' + mode + '"] .mode-icon')) == null ? void 0 : _a.cloneNode(true);
+      if (!icon) {
+        return;
+      }
+      icon.setAttribute("id", "modeIcon");
+      (_b = document.querySelector("#modeIcon")) == null ? void 0 : _b.replaceWith(icon);
+    }
+    setMode(value) {
+      if (value === "auto") {
+        value = _ModeToggle.getPreferredMode();
+      }
+      console.debug(`Switch to ${value} mode`);
+      document.documentElement.setAttribute("data-bs-theme", value);
+      const event = new CustomEvent("hbs:mode", { detail: { mode: value } });
+      document.dispatchEvent(event);
+    }
+    static getPreferredMode() {
+      if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+        return "dark";
+      }
+      return "light";
+    }
+  };
+  var mode_default2 = ModeToggle;
+
+  // ns-hugo-imp:/Users/martijnvandenboom/Library/Caches/hugo_cache/modules/filecache/modules/pkg/mod/github.com/razonyang/hugo-theme-bootstrap@v1.12.0/assets/js/palettes/index.ts
+  var PaletteSelector = class {
+    constructor() {
+      __publicField(this, "key", "palette");
+    }
+    run() {
+      const palette = this.getPalette();
+      if (palette) {
+        this.setPalette(palette);
+      }
+      window.addEventListener("load", () => {
+        this.initPalette();
+      });
+    }
+    initPalette() {
+      const selected = this.getPalette();
+      document.querySelectorAll(".palette").forEach((element) => {
+        const paletteId = element.getAttribute("id").replace("palette-", "");
+        if (paletteId === selected) {
+          element.classList.add("active");
+        }
+        element.addEventListener("click", () => {
+          this.setPalette(paletteId);
+          document.querySelector(".palette.active").classList.remove("active");
+          element.classList.add("active");
+        });
+      });
+    }
+    getPalette() {
+      const palette = local_storage_default2.getItem(this.key);
+      if (palette) {
+        return palette;
+      }
+      const paletteMeta = document.documentElement.getAttribute("data-palette");
+      if (paletteMeta) {
+        return paletteMeta;
+      }
+      return "";
+    }
+    setPalette(palette) {
+      console.debug(`switch to palette: ${palette}`);
+      document.documentElement.setAttribute("data-palette", palette);
+      local_storage_default2.setItem(this.key, palette);
+    }
+  };
+  var palettes_default = PaletteSelector;
+
+  // <stdin>
+  new mode_default2().run();
+  new palettes_default().run();
+})();
