c98f35a18b7524f15b46c4a98701fef701ec9ed5
ing. M.A.C.M. (Martijn) van den Boom <martijn.vandenboom@icloud.com>
2025-02-21 10:30:21 +0100

    attempt to recover theme 1st
    attempt to recover theme 1st

---
+++ themes/hugo-theme-bootstrap/assets/icons/icons/index.js
@@ -0,0 +1,7 @@
+import faBilibili from "./faBilibili";
+import faLiberapay from "./faLiberapay";
+import faOffline from "./faOffline";
+import faTipeee from "./faTipeee";
+import faXTwitter from "./faXTwitter"
+
+export { faBilibili, faLiberapay, faOffline, faTipeee, faXTwitter };
